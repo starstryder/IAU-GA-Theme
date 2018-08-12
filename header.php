142e92bc2bd3b1e78acaabf889980d83ff56f8f7
starstryder <starstryder@gmail.com>
2018-08-12 17:29:54 -0500

    fixing theme name

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <html>
 <head>
     <title>Astronomy 2018 Newspaper</title>
-    <link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/style.css">
+    <link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/IAU-GA-theme/style.css">
 </head>
 <html>
 <body>
 
 <div id="header">
-    <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/images/banner_IAUGA_s.jpg">
+    <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA-theme/images/banner_IAUGA_s.jpg">
 </div>
--- home.php
+++ home.php
@@ -14,7 +14,7 @@ define( 'WP_USE_THEMES', false ); get_header(); ?>
 
         <div style="max-width: 990px;">
             <p style="margin: 0 auto;">
-                <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/images/IAU-NP-Header.png" style="max-height: 75px; margin: 0 auto; width: 100%; ">
+                <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA-Theme/images/IAU-NP-Header.png" style="max-height: 75px; margin: 0 auto; width: 100%; ">
             </p>
         </div>
         <?php wp_nav_menu( array( 'theme_location' => 'section-menu' ) ); ?>
