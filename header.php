27420f69afa3b1265a3f3664fa2b23e5a34eb0ac
starstryder <starstryder@gmail.com>
2018-08-12 17:20:39 -0500

    adding site_url to images and header

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <html>
 <head>
     <title>Astronomy 2018 Newspaper</title>
-    <link rel="stylesheet" href="/wordpress/wp-content/themes/IAU-GA/style.css">
+    <link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/style.css">
 </head>
 <html>
 <body>
 
 <div id="header">
-    <img src="/wordpress/wp-content/themes/IAU-GA/images/banner_IAUGA_s.jpg">
+    <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/images/banner_IAUGA_s.jpg">
 </div>
--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <aside>
     <div id="sidebar">
-        <img src="/wordpress/wp-content/themes/IAU-GA/images/uniwien_logo_klein.png" style="width: 180px; margin: 20px 10px; ">
+        <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/images/uniwien_logo_klein.png" style="width: 180px; margin: 20px 10px; ">
         <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-        <img src="/wordpress/wp-content/themes/IAU-GA/images/Logo_1608_200x280.jpg" style="margin-top: 30px;">
+        <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA/images/Logo_1608_200x280.jpg" style="margin-top: 30px;">
     </div>
 </aside>
 
