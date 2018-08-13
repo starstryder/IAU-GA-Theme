d4c5e52e6791c18f18c0b034bb0625a34430e0b7
starstryder <starstryder@gmail.com>
2018-08-12 21:03:13 -0500

    fixing images

--- functions.php
+++ functions.php
@@ -15,3 +15,16 @@ function register_my_menus() {
     );
 }
 add_action( 'init', 'register_my_menus' );
+
+
+add_image_size( 'Image-Main', 300, 300, true );
+add_image_Size( 'Bio', 150, 150, true );
+
+add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+function my_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'image-main' => __( 'Image-Main' ),
+        'bio' => __( 'Bio' ),
+    ) );
+}
+add_theme_support( 'post-thumbnails' );
