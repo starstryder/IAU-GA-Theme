59f3d265bb016ce88ad172e42e4adfdbb6443933
starstryder <starstryder@gmail.com>
2018-08-12 20:08:15 -0500

    adding weather

--- home.php
+++ home.php
@@ -8,7 +8,15 @@
 
         <div id="post">
             This is the home page
+
+
+            <div class="weather" style="float:right;">
+                <a href="https://www.accuweather.com/en/at/vienna/31868/weather-forecast/31868" class="aw-widget-legal">
+                    <!--
+                    By accessing and/or using this code snippet, you agree to AccuWeather’s terms and conditions (in English) which can be found at https://www.accuweather.com/en/free-weather-widgets/terms and AccuWeather’s Privacy Statement (in English) which can be found at https://www.accuweather.com/en/privacy.
+                    -->
+                </a><div id="awcc1534122270664" class="aw-widget-current"  data-locationkey="31868" data-unit="c" data-language="en-us" data-useip="false" data-uid="awcc1534122270664"></div><script type="text/javascript" src="https://oap.accuweather.com/launch.js"></script>        </div>
+            </div>
+
         </div>
-
-
 <?php get_footer(); ?>
