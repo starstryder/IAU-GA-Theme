5ecd7a2fd84e6f84f7e2fb78a3395970bfd49aaf
starstryder <starstryder@gmail.com>
2018-08-13 00:10:29 -0500

    working on home page

--- footer.php
+++ footer.php
@@ -21,6 +21,17 @@
 
 </div>
 </div>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123832667-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-123832667-1');
+</script>
+
 </body>
 </html>
 
--- home.php
+++ home.php
@@ -8,16 +8,56 @@
 
         <div id="post">
 
-            <div class="IAU">
+            <!---- Sections: Around IAU ------------------------------------------------>
+            <div class="Around">
                 <h1>Around IAU</h1>
                 <?php
-                $get_AroundIAU = new WP_Query( 'category_slug=IAU&posts_per_page=4' );
+                $get_AroundIAU = new WP_Query( 'category_name=IAU&posts_per_page=4' );
 
                 while ( $get_AroundIAU->have_posts() ) : $get_AroundIAU->the_post(); ?>
-                    <div class="home-FM-excerpt"> <?php
+                    <div class="home-excerpt"> <?php
                         the_title('<h3>', '</h3>' );
                         the_excerpt(); ?>
                     </div> <?php
+                endwhile;
+                ?>
+
+                <h1>Around Vienna</h1>
+                <?php
+                $get_AroundVienna = new WP_Query( 'category_name=vienna&posts_per_page=4' );
+
+                while ( $get_AroundVienna->have_posts() ) : $get_AroundVienna->the_post(); ?>
+                    <div class="home-excerpt"> <?php
+                        the_title('<h3>', '</h3>' );
+                        the_excerpt(); ?>
+                    </div> <?php
+                endwhile;
+                ?>
+            </div>
+
+            <!---- Sections: Highlights ------------------------------------------------>
+            <div class="Highlights">
+                <h1>Highlights</h1>
+                <?php
+                $get_AroundIAU = new WP_Query( 'category_name=featured&posts_per_page=4' );
+
+                $i = 0;
+                while ( $get_AroundIAU->have_posts() ) : $get_AroundIAU->the_post();
+                    $i = 0;
+
+                    if ( has_post_thumbnail() && $i == 0 ) {
+                        ?> <div class="home-excerpt"> <?php
+                            the_post_thumbnail($size= 'thumbnail' );
+                            $i++;
+                            the_title('<h3>', '</h3>' );
+                            the_excerpt();
+                            ?> </div> <?php
+                        } else {
+                        ?> <div class="home-excerpt"> <?php
+                            the_title('<h3>', '</h3>' );
+                            the_excerpt();
+                            ?> </div> <?php
+                        }
                 endwhile;
                 ?>
             </div>
