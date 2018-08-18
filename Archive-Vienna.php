740b92ced9df16f81b280dbc2c2e2d271f7cd96b
starstryder <starstryder@gmail.com>
2018-08-18 16:37:17 +0200

    Beginning Archive formats

---
+++ Archive-Vienna.php
@@ -0,0 +1,23 @@
+<?php /**
+ * Template Name: Archive - IAU
+ *
+ * @package WordPress
+ */ ?>
+
+<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
+
+
+
+    <div id="post">
+        <?php the_post(); ?>
+        <h2 class="entry-title" style="margin-bottom: 20px"><?php the_title(); ?> Archive</h2>
+
+        <ul>
+            <?php wp_get_archives('type=alpha', 'cat=7'); ?>
+        </ul>
+
+    </div>
+    </div>
+
+
+<?php get_footer(); ?>
