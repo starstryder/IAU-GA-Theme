d5e98e55a8a8c3ee9e3a580d2c42f8b062b472d6
starstryder <starstryder@gmail.com>
2018-08-19 23:54:07 +0200

    Homepage edit

--- home.php
+++ home.php
@@ -65,7 +65,7 @@
                         <a href="<?php the_permalink(); ?>">
                             <?php if (has_post_thumbnail( $post->ID ) ): ?>
                                 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                                <p><img src="<?php echo $image[0]; ?>" style="max-width:190px; margin:0; padding: 0;"></p>
+                                <p><img src="<?php echo $image[0]; ?>" style="max-width:135px; margin:0; padding: 0;"></p>
                             <?php endif; ?>
 
                             <?php the_title('<h3>', '</h3>' ); ?>
