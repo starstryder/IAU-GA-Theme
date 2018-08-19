03342a9b1900c5c7ac41891cf8300e6d41e56e51
starstryder <starstryder@gmail.com>
2018-08-19 22:50:36 +0200

    Homepage edit

--- home.php
+++ home.php
@@ -58,7 +58,12 @@
             </div>
 
             <!---- Sections: Focus Meetings ------------------------------------------------>
-
+            <div class="FM-bar">
+                <h1>Focus Meetings - Coming Soon</h1>
+            </div>
+            <div class="Sym">
+                <h1>Symposia</h1>
+            </div>
 
 
 
