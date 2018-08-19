1e8180528ef24505eb3c67a293bef419914ab327
starstryder <starstryder@gmail.com>
2018-08-20 01:20:21 +0200

    Homepage edit

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 
 <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 
-        <div id="post" style="height: 2000px">
+        <div id="post" style="height: 1700px">
 
             <!---- Sections: Around IAU ------------------------------------------------>
             <div class="Around">
