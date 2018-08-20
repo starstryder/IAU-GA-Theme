42aab0dbab3d87b12f4e48db8bcbc240b8b03011
starstryder <starstryder@gmail.com>
2018-08-20 10:13:08 +0200

    Homepage edit

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
 
        <div id="post">
            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
                <h2><?php the_title(); ?></h2>
-               <?php the_meta(); ?>
+               <ul class="post-meta" style="margin:0 0 10px 0;">
+                   <?php
+                   $author = get_post_custom_values("written by");
+                   echo "<li>written by: ". $author[0]. "</li>";
+                   $when = get_post_custom_values("when");
+                   echo "<li>when: ". $when[0]. " <span style='font-style: italic; font-weight: normal; font-size: 90%;'>&nbsp; &nbsp; &nbsp; check app for details.</span></li>";
+                   ?>
+               </ul>
+
                <?php the_content(__('(more...)')); ?>
 
            <?php endwhile; else: ?>
