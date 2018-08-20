6f819012bacaebb2a960a1179c07ad1e03822d09
starstryder <starstryder@gmail.com>
2018-08-20 11:50:09 +0200

    Homepage edit

--- category.php
+++ category.php
@@ -10,17 +10,23 @@
 
 <div id="post">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <ul class="post-meta" style="margin:0 0 10px 0;">
+        <ul class="post-meta" style="margin:0;">
             <div class="home-excerpt">
+                <a href="<?php the_permalink(); ?>">
+                    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                        <img src="<?php echo $image[0]; ?>" style="max-width:100px; margin:0 10px 10px 0; padding: 0; float: left;">
+                    <?php endif; ?>
+                    <?php
+                    the_title('<h4>', '</h4>' );?>
+                </a>
                 <?php
                 $author = get_post_custom_values("written by");
                 echo "<li>written by: ". $author[0]. "</li>";
                 $when = get_post_custom_values("when");
                 echo "<li>when: ". $when[0]. " <span style='font-style: italic; font-weight: normal; font-size: 90%;'>&nbsp; &nbsp; &nbsp; check app for details.</span></li>";
                 ?>
-                <a href="<?php the_permalink(); ?>"><?php
-                    the_title('<h4>', '</h4>' );?>
-                </a> <?php
+                <?php
                 the_excerpt(); ?>
             </div>
         </ul>
