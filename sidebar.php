2db244294663b9eb2270fbc4384ebd722fce6abf
starstryder <starstryder@gmail.com>
2018-08-20 13:06:10 +0200

    Homepage edit

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,11 @@
     <div id="sidebar">
         <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA-Theme/images/uniwien_logo_klein.png" style="width: 180px; margin: 20px 10px; ">
         <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        <div id ="search" style="margin: 5px;">
+            <?php
+                get_search_form();
+            ?>
+        </div>
         <img src="<?php echo site_url(); ?>/wp-content/themes/IAU-GA-Theme/images/Logo_1608_200x280.jpg" style="margin-top: 30px;">
     </div>
 </aside>
