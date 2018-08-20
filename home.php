2025ffe6f46e2cc8e6a04596f73598c31a0a189a
starstryder <starstryder@gmail.com>
2018-08-20 11:38:29 +0200

    Homepage edit

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
             <div class="Around">
                 <h1>Around the GA</h1>
                 <?php
-                $get_AroundIAU = new WP_Query( 'cat=1,-9' );
+                $get_AroundIAU = new WP_Query( 'cat=1,-9&posts_per_page=3' );
 
                 while ( $get_AroundIAU->have_posts() ) : $get_AroundIAU->the_post(); ?>
                     <div class="home-excerpt">
@@ -26,7 +26,7 @@
 
                 <h1>Around Vienna</h1>
                 <?php
-                $get_AroundVienna = new WP_Query(  'cat=7,-9'  );
+                $get_AroundVienna = new WP_Query(  'cat=7,-9&posts_per_page=3'  );
 
                 while ( $get_AroundVienna->have_posts() ) : $get_AroundVienna->the_post(); ?>
                     <div class="home-excerpt">
