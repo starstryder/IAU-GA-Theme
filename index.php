55085e67b52ebf65db1465e3e30f720dcae8b95e
starstryder <starstryder@gmail.com>
2018-08-21 12:23:59 +0200

    Homepage edit

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
                <ul class="post-meta" style="margin:0 0 10px 0;">
                    <?php
                    $author = get_post_custom_values("written by");
-                   echo "<li>written by: ". $author[0]. "</li>";
+                   if (strlen($author[0]) > 2)
+                       echo "<li>written by: ". $author[0]. "</li>";
                    $when = get_post_custom_values("when");
                    if (strlen($when[0]) > 2)
                         echo "<li>when: ". $when[0]. " <span style='font-style: italic; font-weight: normal; font-size: 90%;'>&nbsp; &nbsp; &nbsp; check app for details</span></li>";
