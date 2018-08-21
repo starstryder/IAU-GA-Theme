36570f7a2478fb5082caeacb2e431941ff536486
starstryder <starstryder@gmail.com>
2018-08-21 17:42:49 +0200

    Homepage edit

--- archive.php
+++ archive.php
@@ -10,26 +10,19 @@
 
 <div id="post">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <ul class="post-meta" style="margin:0;">
-            <div class="home-excerpt">
-                <a href="<?php the_permalink(); ?>">
-                    <?php if (has_post_thumbnail( $post->ID ) ): ?>
-                        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                        <img src="<?php echo $image[0]; ?>" style="max-width:150px; margin:0 10px 10px 0; padding: 0; float: right;">
-                    <?php endif; ?>
-                    <?php
-                    the_title('<h4>', '</h4>' );?>
-                </a>
-                <?php
-                $author = get_post_custom_values("written by");
-                echo "<li>written by: ". $author[0]. "</li>";
-                $when = get_post_custom_values("when");
-                if (strlen($when[0]) > 2)
-                    echo "<li>when: ". $when[0]. " <span style='font-style: italic; font-weight: normal; font-size: 90%;'>&nbsp; &nbsp; &nbsp; check app for details.</span></li>";
-                ?>
-                <?php
-                the_excerpt(); ?>
-            </div>
+
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+
+        <?php get_search_form(); ?>
+
+        <h2>Archives by Month:</h2>
+        <ul>
+            <?php wp_get_archives('type=monthly'); ?>
+        </ul>
+
+        <h2>Archives by Subject:</h2>
+        <ul>
+            <?php wp_list_categories(); ?>
         </ul>
         <div style="clear:both;"></div>
 
